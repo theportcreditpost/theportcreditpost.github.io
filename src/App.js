587d1f460ede2908c0e7d795a393b1010a8143e1
Abeer05 <malikabeer23@gmail.com>
2024-03-31 16:05:49 -0400

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <Header />
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path="/" exact />
         </Routes>
       </Router>
     </>
