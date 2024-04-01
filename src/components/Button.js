27bff0258e535b6985d41bf7cfa442eec208c103
Abeer05 <malikabeer23@gmail.com>
2024-03-31 22:17:19 -0400

    Created navbar

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 import { Link } from "react-router-dom";
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize}) => {
-    
-    return (
-        <Link to='/' className='btn-mobile'>
-            <button className='btn' onClick={onClick} type={type}>
-                {children}
-            </button>
-        </Link>
-    )
+export const Button = ({ children, type, onClick }) => {
+  return (
+    <Link to="/" className="btn-mobile">
+      <button className="btn" onClick={onClick} type={type}>
+        {children}
+      </button>
+    </Link>
+  );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './img/logo.png';
-import title from './img/title.png'
-import './Header.css'
+import React from "react";
+import logo from "./img/logo.png";
+import title from "./img/title.png";
+import "./Header.css";
 
 function Header() {
-    return (
-        <>
-          <div className="img-container">
-            <img src={logo} alt='Lighthouse Logo' id='logo' />
-            <img src={title} alt='The Port Credit Post' id='title'/>
-          </div>
-          <p class="text">
-            <span> JANUARY NEWSLETTER OUT NOW!</span>
-          </p>
-          <p class="text text2">
-            <span> SEE ARTICLES AND IMAGES BELOW!</span>
-          </p>
-        </>
-    );
+  return (
+    <>
+      <div className="img-container">
+        <img src={logo} alt="Lighthouse Logo" id="logo" />
+        <img src={title} alt="The Port Credit Post" id="title" />
+      </div>
+      <p className="text">
+        <span> JANUARY NEWSLETTER OUT NOW!</span>
+      </p>
+      <p className="text text2">
+        <span> SEE ARTICLES AND IMAGES BELOW!</span>
+      </p>
+    </>
+  );
 }
 
 export default Header;
